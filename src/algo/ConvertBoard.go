290f37697fd53fe3dbba3bbf7295a034b1e6a893
Teodor-andrei BALEA <tbalea@e1r12p6.42.fr>
2017-01-23 17:01:39 +0100

    Solvabilty done

--- src/algo/ConvertBoard.go
+++ src/algo/ConvertBoard.go
@@ -12,7 +12,7 @@
 
 package algo
 
-import "fmt"
+/*import "fmt"*/
 
 var (
 	obv   int
@@ -114,38 +114,63 @@ func SetObjectifBoard(long, large int) [][]int {
 	return (objtf)
 }
 
-//	*	Check solvability by odd/even inversion for initial state and final state
+//	*	Check solvability by even/even inversion for initial state and final state
 func CheckSolvability(board [][]int, long, large int) [][]int {
 	var final [][]int
+	var even int
 
 	final = SetObjectifBoard(long, large)
+	even = BoolToInt(large%2 == 0) * large * long
 	ConvertToSnail(final, long, large)
-	if CheckInversion(BoardToString(board, long, large)) == CheckInversion(BoardToString(final, long, large)) {
-		ConvertToRight(final, long, large)
-		return final
-	} else {
-		//TODO Check (Inversion final) != (Inversion first) even if it's solvable
-		fmt.Println("TODOCheck inversion 1 :", CheckInversion(BoardToString(board, long, large)), "check inversion 2 :", CheckInversion(BoardToString(final, long, large)))
+	/*ConvertToSnail(board, long, large)
+	fmt.Println("Size =", even, "\nfinal = {", BoardToString(final, long, large), "}\nboard = {", BoardToString(board, long, large), "}")
+	fmt.Println("TODOCheck inversion 1 :", CheckInversion(BoardToString(board, long, large), even, large), "check inversion 2 :", CheckInversion(BoardToString(final, long, large), even, large))*/
+	if CheckInversion(BoardToString(board, long, large), even, large) == CheckInversion(BoardToString(final, long, large), even, large) {
 		ConvertToRight(final, long, large)
 		return final
 	}
+	/* else {
+		//TODO Check (Inversion final) != (Inversion first) even if it's solvable
+		fmt.Println("TODOCheck inversion 1 :", CheckInversion(BoardToString(board, long, large), even, large), "check inversion 2 :", CheckInversion(BoardToString(final, long, large), even, large))
+		ConvertToRight(board, long, large)
+		return final
+	}*/
 	return nil
 }
 
 //	*	Check number of inversion
-func CheckInversion(check []int) bool {
-	var li, i, max, r int
+func CheckInversion(check []int, even int, large int) bool {
+	var i, max, r, j, iseven int
 
-	i, li, r = 0, -1, 0
+	i, r, iseven = 0, 0, 0
 	max = len(check)
 	for i < max {
-		if check[i] == li+1 {
-			check[i], check[li], i, li = check[li], check[i], li, -1
-			r++
-		} else if li < 0 && check[i] != i+1 {
-			li = i
+		if check[i] == even && iseven == 0 && even > 0 {
+			iseven = i / large
+			/*fmt.Println("iseven =", iseven)*/
+		}
+		if check[i] != large*large {
+			for j = 1; j+i < max; j++ {
+				if check[i] > check[i+j] {
+					r++
+				}
+			}
 		}
 		i++
 	}
-	return ((r % 2) == 0)
+	return (((r + iseven) % 2) == 0)
 }
+
+// Returns true if `start` belongs to the same permutation group as `goal`
+/*template <uint size>
+bool isSolvable(const Puzzle<size> & start, const Puzzle<size> & goal) {
+	auto startInversions = inversions(start);
+	auto goalInversions = inversions(goal);
+
+	if (size % 2 == 0) { // In this case, the row of the '0' tile matters
+		startInversions += start.indexOf(0) / size;
+		goalInversions += goal.indexOf(0) / size;
+	}
+
+	return (startInversions % 2 == goalInversions % 2);
+}*/
